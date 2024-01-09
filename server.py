dcaa92eebc2165000b153a1d2cce26c14286ffc3
Partha <parthasdeyofficial@gmail.com>
2024-01-09 23:53:14 +0530

    initial commit

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<a href='/login'>LOGIN PAGE</a>"
+    return render_template('homepage.html')
 
 @app.route('/login')
 def login():
